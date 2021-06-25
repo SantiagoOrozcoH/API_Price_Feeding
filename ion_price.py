f7c7b3685ed4c91a9a7f033a55d3eae26064d619
Santiago Orozco H <santiago.orozcoh@udea.edu.co>
2021-06-25 00:10:16 -0500

    .ignore added and naming of csv file changed

--- ion_price.py
+++ ion_price.py
@@ -3,15 +3,19 @@ import json
 import time
 import csv
 
-url = "https://api-osmosis.imperator.co/search/v1/price/ion"
+url_ion_price = "https://api-osmosis.imperator.co/search/v1/price/ion"
+url_osmo_price = "https://api-osmosis.imperator.co/search/v1/price/osmo"
 
+url_pool_ion_osmo_amount= "https://api-osmosis.imperator.co/search/v1/pools/2"   #80%-20%
+url_pool_ion_osmo_amount= "https://api-osmosis.imperator.co/search/v1/pools/11"  #50%-50%
+url_pool_atom_ion_amount= "https://api-osmosis.imperator.co/search/v1/pools/14"  #50%-50%
 
 def price_extract(url_):
     r = requests.get(url_)
     r_json = r.json()
-    asset_price = r_json["price"]
-    asset_price = float(asset_price) / 10
-    asset_price = round(asset_price, 3)
+    # asset_price = r_json["price"]
+    # asset_price = float(asset_price)                #for some reason there is a x10 in the value
+    asset_price = round(float(r_json["price"]) , 3) / 10
     return asset_price
 
 def write_csv(file_name, data_):
@@ -25,10 +29,12 @@ def get_hour():
     return date_str
 
 
-last_price = price_extract(url)
+last_price = price_extract(url_ion_price)
+
+time_div = 30       #Seconds
 
 while(1):
-    p = price_extract(url)
+    p = price_extract(url_ion_price)
     
     if (p != last_price):
         if ((p - last_price)>0):
@@ -40,7 +46,8 @@ while(1):
     last_price = p
 
     data = str(time.time())+", "+str(p)+"\n"
-    write_csv("ion_price.csv", data)
+    f_name = "ion_price_"+str(time_div)+"s.csv"
+    write_csv(f_name, data)
 
-    time.sleep(5.00)
+    time.sleep(time_div)
    
